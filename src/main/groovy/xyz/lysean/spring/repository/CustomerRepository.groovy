c9220709fc1de0b9bf16f5564be24fd8786385fd
liuyongsheng <liuyongsheng@sicent.com>
2016-08-09 15:49:21 +0800

    重构目录，并创建jenkins orchly-plugin 模板

--- src/main/groovy/xyz/lysean/spring/repository/CustomerRepository.groovy
+++
@@ -1,8 +0,0 @@
-package xyz.lysean.spring.repository
-
-import org.springframework.data.repository.CrudRepository
-import xyz.lysean.spring.domain.Customer
-
-interface CustomerRepository extends CrudRepository<Customer, Long> {
-
-}
--- src/main/groovy/xyz/lysean/spring/repository/RoomRepository.groovy
+++
@@ -1,7 +0,0 @@
-package xyz.lysean.spring.repository
-
-import org.springframework.data.repository.CrudRepository
-import xyz.lysean.spring.domain.Room
-
-interface RoomRepository extends CrudRepository<Room, Long> {
-}
